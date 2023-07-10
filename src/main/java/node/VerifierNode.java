5c183322889cdd6baa25bb969c98992e204f5790
Kyle Standing <kjstanding2@gmail.com>
2023-07-10 11:35:27 -0600

    Add ML Files

---
+++ src/main/java/node/VerifierNode.java
@@ -0,0 +1,8 @@
+package node;
+
+public class VerifierNode {
+
+    public void verify(){
+        // Needs a starting and a final model, dataset to train with, and instructions on how to train.
+    }
+}
